e7d21264be585f3fc345d44a8f5f1d8363aa1fbb
julianmendoza <julianjohnmendoza@gmail.com>
2021-11-08 14:18:01 -0500

    sendhelp

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class test {
+    private JList list1;
+    private JList list2;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+}
